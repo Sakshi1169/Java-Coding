1134ace0c58b3f31c92e203feae77022d66a3911
Sakshi Shelke <Sakshishelke316@gmail.com>
2026-05-08 19:19:50 +0530

    Largest Element in Array

---
+++ src/Array/LargestElement.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Scanner;
+//Find Largest Element in Array
+
+public class LargestElement {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Size");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
+        int largest=arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i]>largest)
+            {
+                largest=arr[i];
+            }
+
+        }
+        System.out.println("Largest Elementin Array is : "+largest);
+    }
+}
